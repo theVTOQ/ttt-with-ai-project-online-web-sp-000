97efb5ab1d2c6786ecd87c65dc998398348404cf
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-26 04:05:16 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,7 @@ class Game
 
   attr_accessor :board, :player_1, :player_2
 
-  def initialize(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
